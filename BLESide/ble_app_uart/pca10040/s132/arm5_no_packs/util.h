d8de33b8e3e3883a6e36e58e9d912e1327f497b0
Amar Duranovic <amard15@student.uia.no>
2017-03-22 11:14:54 +0100

    Added struct

--- BLESide/ble_app_uart/pca10040/s132/arm5_no_packs/util.h
+++ BLESide/ble_app_uart/pca10040/s132/arm5_no_packs/util.h
@@ -1,7 +1,15 @@
 #ifndef __UTIL_H
 #define __UTIL_H
 
+#include "definitions.h"
+#include "util.c"
 
-
+typedef struct Data
+{
+	
+  uint16_t         *d_ptr;       // Data pointer       
+  uint16_t         *next_ptr;    // Pointer of next data     
+	uint16_t				 *prev_ptr;		 // Pointer of previous data
+} dataqueue;
 
 #endif
